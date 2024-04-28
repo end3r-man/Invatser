faa863c21e4c4f5f694c02a9f4cd0158d51d8209
end3r-man <praveen.venkatachalam@proton.me>
2024-04-28 20:49:52 +0530

    DashAna Up

--- app/Livewire/User/DashBoard.php
+++ app/Livewire/User/DashBoard.php
@@ -3,7 +3,9 @@
 namespace App\Livewire\User;
 
 use App\Models\User;
+use App\Models\UserClient;
 use App\Models\UserInvoice;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class DashBoard extends Component
@@ -19,6 +21,12 @@ class DashBoard extends Component
 
     public $mtotal = 0;
 
+    public $buyer = 0;
+
+    public $percen;
+
+    public $mlsale;
+
     public function render()
     {
         return view('livewire.user.dash-board');
@@ -29,34 +37,35 @@ class DashBoard extends Component
 
         $ucom = User::where('id', auth()->user()->id)->first();
 
+        $this->buyer = UserClient::where('active', true)->get();
+
+        $today = Carbon::now()->startOfMonth();
+
+        $pmonth = Carbon::now()->subMonth()->month;
+
+        $bcont = 0;
+
+        $ocont = 0;
+
+        foreach ($this->buyer as $value) {
+            if ($value->created_at->between($today, Carbon::now())) {
+                $bcont++;
+            } else {
+                $ocont++;
+            }
+        }
+
+        $this->percen['cont'] = $bcont;
+        
+        if ($ocont == 0 && $bcont != 0) {
+            $this->percen['per'] = 100;
+        } elseif ($bcont == 0) {
+            $this->percen['per'] = 0;
+        } else {
+            $this->percen['per'] = round(($bcont / $ocont) * 100, 2);
+        }
+
         $this->code = $ucom->currency;
-
-        // $invo = UserInvoice::whereMonth('created_at', date('m'))
-        // ->whereYear('created_at', date('Y'))
-        // ->where('user_id', auth()->user()->id)
-        // ->get();
-
-        // $ytrm = 0;
-
-        // $tmas = 0;
-
-        // foreach ($invo as $key => $value) {
-        //     $js = json_decode($value->product);
-
-        //     foreach ($js as $item) {
-                
-        //         $this->mtotal += $item->total;
-        //         $ytrm += $item->quantity;
-        //         $tmas += $item->total;
-                
-        //         $this->tsales = [
-        //             'name' => $item->product,
-        //             'qty' => $ytrm,
-        //             'total' => $tmas,
-        //         ];
-        //     }
-        // }
-
 
         $invo = UserInvoice::whereMonth('created_at', date('m'))
             ->whereYear('created_at', date('Y'))
@@ -85,11 +94,45 @@ class DashBoard extends Component
             }
         }
 
-
         $this->msales = $productData;
 
-      //  dd($this->msales);
-
         $this->tsales = $this->mtotal;
+
+        $inv2 = UserInvoice::whereMonth('created_at', $today)
+        ->where('user_id', auth()->user()->id)
+        ->get();
+
+        $inv3 = UserInvoice::whereMonth('created_at', $pmonth)
+        ->where('user_id', auth()->user()->id)
+        ->get();
+
+        $pldata = 0;
+        $mltsotal = 0;
+
+        foreach ($inv2 as $key => $value) {
+            $js = json_decode($value->product);
+
+            foreach ($js as $item) {
+                $mltsotal += $item->total;
+            }
+        }
+
+        foreach ($inv3 as $key => $value) {
+            $js = json_decode($value->product);
+
+            foreach ($js as $item) {
+                $pldata += $item->total;
+            }
+        }
+
+        $this->mlsale['cont'] = $mltsotal;
+        
+        if ($pldata == 0 && $mltsotal != 0) {
+            $this->mlsale['per'] = 100;
+        } elseif ($mltsotal == 0) {
+            $this->mlsale['per'] = 0;
+        } else {
+            $this->mlsale['per'] = round(($mltsotal / $pldata) * 100, 2);
+        }
     }
 }
