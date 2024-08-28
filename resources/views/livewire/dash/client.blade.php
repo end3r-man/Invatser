5eaa94899687146dd30af8b048c75ca600c55b70
end3r-man <praveen.venkatachalam@proton.me>
2024-08-28 23:34:12 +0530

    🗑️ Client Table Rm

--- resources/views/livewire/dash/client.blade.php
+++ resources/views/livewire/dash/client.blade.php
@@ -59,12 +59,5 @@
                 <iconify-icon class="text-white text-2xl" icon="ph:text-columns-duotone"></iconify-icon>
             </div>
         </div>
-
-        {{-- User Table --}}
-        <table>
-            <thead>
-                <td></td>
-            </thead>
-        </table>
     </div>
 </div>
